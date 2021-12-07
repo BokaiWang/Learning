f2dc616c7a261853271d3c8f2bee5c8c98a5c729
BokaiWang <bk.wang1989@gmail.com>
2021-12-07 18:46:45 -0500

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by 王柏凱 on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
