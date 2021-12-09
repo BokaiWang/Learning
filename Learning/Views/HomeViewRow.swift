bb1312ba40652dfeebbcafdf646e48f07c6b9e8f
BokaiWang <bk.wang1989@gmail.com>
2021-12-09 16:42:11 -0500

    - Create a reusable view of a card - Put the card in HomeView

---
+++ Learning/Views/HomeViewRow.swift
@@ -0,0 +1,71 @@
+//
+//  HomeViewRow.swift
+//  Learning
+//
+//  Created by 王柏凱 on 2021-12-08.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    var image:String
+    var title:String
+    var description:String
+    var count:String
+    var time:String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            
+            HStack {
+                // Image
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                Spacer()
+                // Text
+                VStack(alignment: .leading, spacing: 10) {
+                    // Headline
+                    Text(title)
+                        .bold()
+                    // Description
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    
+                    // Icons
+                    HStack {
+                        
+                        // Number of lessons/questions
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(Font.system(size: 10))
+                        Spacer()
+                        // Time
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(.caption)
+                    }
+                } // VStack
+                .padding(.leading, 20)
+            } // HStack
+            .padding([.horizontal], 20)
+        } // ZStack
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn Swift", description: "some description", count: "10 Lessons", time: "2 Hours")
+    }
+}
