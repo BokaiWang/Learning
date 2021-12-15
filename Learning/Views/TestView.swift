9bfde2eab74ece1acf6995439742e12e976e80ac
BokaiWang <bk.wang1989@gmail.com>
2021-12-15 18:25:27 -0500

    - Add TestResultView - Show TestResultView in the TextView after the last question

--- Learning/Views/TestView.swift
+++ Learning/Views/TestView.swift
@@ -100,8 +100,9 @@ struct TestView: View {
             .navigationTitle("\(model.currentModule?.category ?? "") Test")
         }
         else {
-            // Test hasn't loaded yet
-            ProgressView()
+            // If current question is nil, show the result view
+            TestResultView(numCorrect: numCorret)
+            
         }
     }
     
